cacbc74cebdec0a21c6dc0d472e75c8f8d563cfc
waltermichelraja <waltermichelraja@gmail.com>
2025-09-06 14:10:03 +0530

    add cookie_settings

--- backend/authentication/apps.py
+++ backend/authentication/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class AuthenticationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    default_auto_field='django.db.models.BigAutoField'
+    name='authentication'
+
+    COOKIE_SETTINGS={
+    "httponly": True,
+    "secure": True,
+    "samesite": "Strict",
+    "path": "/api/auth/"
+}
