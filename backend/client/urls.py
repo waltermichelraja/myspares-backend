54157c33b8f989c449f312cb4893398093426d89
waltermichelraja <waltermichelraja@gmail.com>
2025-09-19 22:19:15 +0530

    feat(client/<user_id>/cart/): .views

--- backend/client/urls.py
+++ backend/client/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import list_cart, add_to_cart, remove_from_cart
 
-urlpatterns=[
-    
+urlpatterns = [
+    path("<str:user_id>/cart/", list_cart, name="list_cart"),
+    path("<str:user_id>/cart/add/<str:product_id>/", add_to_cart, name="add_to_cart"),
+    path("<str:user_id>/cart/remove/<str:product_id>/", remove_from_cart, name="remove_from_cart"),
 ]
