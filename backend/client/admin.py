efe3f982d1fc8e637f0c8b40b8a5ea7ebc59170b
waltermichelraja <waltermichelraja@gmail.com>
2025-09-21 01:28:55 +0530

    feat(MONGO_DB): models -> addresses

--- backend/client/admin.py
+++ backend/client/admin.py
@@ -2,5 +2,6 @@ from django.conf import settings
 
 
 users_collection=settings.MONGO_DB["users"]
+addresses_collection=settings.MONGO_DB["addresses"]
 products_collection=settings.MONGO_DB["products"]
 carts_collection=settings.MONGO_DB["carts"]
