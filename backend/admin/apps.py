504dd81f82bd31b12aa45759c93e9cb5525d7b25
waltermichelraja <waltermichelraja@gmail.com>
2025-10-04 19:15:36 +0530

    ops(ready): development mode -> audit logs

--- backend/admin/apps.py
+++ backend/admin/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+import os
 
 
 class AdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin'
+    default_auto_field='django.db.models.BigAutoField'
+    name='admin'
+
+    def ready(self):
+        if os.environ.get("RUN_MAIN")!="true":
+            return
+        from .events import start_watchers
+        start_watchers()
