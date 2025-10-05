895adec7df9b51240391a9230ed0d87b2cdeebfe
waltermichelraja <waltermichelraja@gmail.com>
2025-10-05 15:24:41 +0530

    refactor: create logs authentication_events

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,4 @@
+import logging
 from django.conf import settings
 
 
@@ -5,3 +6,12 @@ users_collection=settings.MONGO_DB["users"]
 users_collection.create_index("phone_number", unique=True)
 
 blacklisted_tokens_collection=settings.MONGO_DB["blacklisted_tokens"]
+
+logger=logging.getLogger("authentication_events")
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    ch=logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter=logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
