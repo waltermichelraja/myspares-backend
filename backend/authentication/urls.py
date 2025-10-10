9708c67b684f8c100031ac303392d8b3484bd259
waltermichelraja <waltermichelraja@gmail.com>
2025-10-10 14:07:23 +0530

    feat(auth/register/): add send_otp_view, verify_otp_view

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import register, login, refresh, logout
+from .views import register, send_otp_view, verify_otp_view, login, refresh, logout
 
 urlpatterns = [
     path("register/", register, name="register"),
+    path("register/send-otp/", send_otp_view, name="send_otp"),
+    path("register/verify-otp/", verify_otp_view, name="verify_otp"),
     path("login/", login, name="login"),
     path("refresh/", refresh, name="token_refresh"),
     path("logout/", logout, name="logout"),
