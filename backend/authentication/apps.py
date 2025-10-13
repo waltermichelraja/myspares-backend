e343da93b2492c8d879463e278685c43223981d1
waltermichelraja <waltermichelraja@gmail.com>
2025-10-13 19:13:57 +0530

    refactor: rewrite AuthenticationConfig

--- backend/authentication/apps.py
+++ backend/authentication/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-import os
-
 
 class AuthenticationConfig(AppConfig):
     default_auto_field='django.db.models.BigAutoField'
@@ -12,12 +10,3 @@ class AuthenticationConfig(AppConfig):
     "samesite": "Strict",
     "path": "/api/auth/"
 }
-
-    def ready(self):
-        if os.environ.get("RUN_MAIN")!="true":
-            return
-        from .models import TokenManager
-        try:
-            TokenManager.cleanup_old_tokens(days=14)
-        except Exception as e:
-            print(f"[Startup Error] failed to cleanup tokens: {e}")
