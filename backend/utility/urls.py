fbf6402ac1a81301cbdc3407ebacfb6c6efa622f
waltermichelraja <waltermichelraja@gmail.com>
2025-10-29 22:05:30 +0530

    refactor: align with DRF convention for query parameter

--- backend/utility/urls.py
+++ backend/utility/urls.py
@@ -8,18 +8,18 @@ from .views import (
 
 urlpatterns=[
     path("brands/", list_brands, name="list_brands"),
+    path("brands/search/", search_brand, name="search_brands"),
     path("brands/<str:brand_code>/", fetch_brand, name="fetch_brand"),
-    path("brands/search/<str:query>/", search_brand, name="search_brands"),
 
     path("brands/<str:brand_code>/models/", list_models, name="list_models"),
+    path("brands/<str:brand_code>/models/search/", search_model, name="search_models"),
     path("brands/<str:brand_code>/models/<str:model_code>/", fetch_model, name="fetch_model"),
-    path("brands/<str:brand_code>/models/search/<str:query>/", search_model, name="search_models"),
 
     path("brands/<str:brand_code>/models/<str:model_code>/categories/", list_categories, name="list_categories"),
+        path("brands/<str:brand_code>/models/<str:model_code>/categories/search/", search_category, name="search_categories"),
     path("brands/<str:brand_code>/models/<str:model_code>/categories/<str:category_code>/", fetch_category, name="fetch_category"),
-    path("brands/<str:brand_code>/models/<str:model_code>/categories/search/<str:query>/", search_category, name="search_categories"),
 
     path("brands/<str:brand_code>/models/<str:model_code>/categories/<str:category_code>/products/", list_products, name="list_products"),
+    path("brands/<str:brand_code>/models/<str:model_code>/categories/<str:category_code>/products/search/", search_product, name="search_products"),
     path("brands/<str:brand_code>/models/<str:model_code>/categories/<str:category_code>/products/<str:product_code>/", fetch_product, name="fetch_product"),
-    path("brands/<str:brand_code>/models/<str:model_code>/categories/<str:category_code>/products/search/<str:query>/", search_product, name="search_products"),
 ]
