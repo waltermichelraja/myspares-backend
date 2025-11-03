2fc0f272a91838ffc9542ef73a57a3bf1e3ead14
waltermichelraja <waltermichelraja@gmail.com>
2025-11-03 19:03:18 +0530

    ops: add ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,8 +11,7 @@ BASE_DIR=Path(__file__).resolve().parent.parent
 
 SECRET_KEY=os.getenv("DJANGO_KEY")
 DEBUG=True
-ALLOWED_HOSTS=[]
-
+ALLOWED_HOSTS=["myspares-backend.onrender.com"]
 MONGO_URI=os.getenv("MONGO_URI")
 MONGO_DB_NAME="MySpares_db"
 client=MongoClient(MONGO_URI)
